a06a760f0c2677367c34fa4c5f0a2ddf90db0af0
Samuel Adema <samiman20212021@gmail.com>
2026-07-14 16:33:35 +0300

    Book Class

--- day04/practice.py
+++ day04/practice.py
@@ -14,3 +14,39 @@ samuel = Account("Sami", 200)
 samuel.deposit(200)
 samuel.withdraw(100)
 samuel.account_statment()
+
+
+
+class Account:
+    def __init__(self, balance):
+        self.__balance = balance
+        
+    def get_balance(self):
+        return self.__balance
+        
+    def withdraw(self, amount):
+        if amount > self.__balance:
+            print("Insufficient Balance")
+        return 
+        self._balance -= amount
+    
+sami = Account(9000)
+sami.get_balance()
+sami.withdraw(500)
+sami.get_balance()  
+        
+
+class Book:
+    def __init__(self, title, author, pages):
+
+        self.title = title
+        self.pages = pages
+        self.author = author
+    def describe(self):
+        print(f"{self.title} is written by {self.author} and has {self.pages} pages.")
+
+Book1 = Book("The Alchemist", "Paulo Coelho", 197)
+Book1.describe()
+
+Book2 = Book("The Great Gatsby", "F. Scott Fitzgerald", 180)
+Book2.describe()
